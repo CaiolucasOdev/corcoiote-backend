099f7bddb037141d6da71fdc63d6606e6cf82088
Caio Lucas <caiolucasodev@gmail.com>
2026-09-10 23:18:57 +0000

    feat: adicionar insercao de novos registros

--- api/src/services/user.service.ts
+++ api/src/services/user.service.ts
@@ -1,5 +1,5 @@
 import users from "../mocks/user.mock.ts";
-import type { User } from "../types/user.type.ts";
+import type { User, CreateUser } from "../types/user.type.ts";
 
 export function findAllUsers(): User[] {
   return users;
@@ -15,11 +15,23 @@ export function findUserById(id: number): User {
  
  }
   
-  
+export function insertUser({
+  name,
+  email,
+  password
+}:CreateUser): User {
 
 
-function insertUser(){
+  const user: User = {
+    id: users[users.length - 1].id +1,
+    name,
+    email,
+    password
+  };
 
+  users[users.length] = user;
+
+  return user;
 }
 
 function modifyUser(){
--- api/src/types/user.type.ts
+++ api/src/types/user.type.ts
@@ -4,3 +4,5 @@ name: string;
 email: string;
 password: string;
 }
+
+export type CreateUser = Omit<User, 'id'>;
