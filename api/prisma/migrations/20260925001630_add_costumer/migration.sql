d9c4229f0fbe7be92e8c6e0bb684938adbbbd449
Caio Lucas <caiolucasodev@gmail.com>
2026-09-25 00:22:55 +0000

    feat: adicionar tabela de clientes.

---
+++ api/prisma/migrations/20260925001630_add_costumer/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "costumers" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "image_url" TEXT,
+
+    CONSTRAINT "costumers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "costumers_email_key" ON "costumers"("email");
